185a1e79519fe19ab42328dbf60987229cbcddf0
IlyaSolonovich <ilyasolonovich20@gmail.com>
2022-11-22 20:24:11 +0100

    aaa

--- ruby-basics-ru/basics/lib/fibonacci.rb
+++ ruby-basics-ru/basics/lib/fibonacci.rb
@@ -1,22 +1,5 @@
 # frozen_string_literal: true
 
 # BEGIN
-module fibonacci
-    def fibonacci num
-        
-        if num === 2
-            1
-        elsif num.zero?
-            0
-        elsif num === 1
-            1
-        elsif num.negative?
-            nil
-        else
-            fibonacci(num - 1) + fibonacci(num - 2)
-        end
-    end
-end
-
 
 # END
--- ruby-basics-ru/basics/lib/fizz_buzz.rb
+++ ruby-basics-ru/basics/lib/fizz_buzz.rb
@@ -1,24 +1,5 @@
 # frozen_string_literal: true
 
 # BEGIN
-module fizz_buzz
-    def fizz_buzz num1, num2
-        acc = ''
-        counter = num1
-        while counter <= num2
-            if (counter % 15).zero?
-                acc += ' ' + 'FizzBuzz'
-            elsif (counter % 5).zero?
-                acc += ' ' + 'Buzz'
-            elsif (counter % 3).zero?
-                acc += ' ' + 'Fizz'
-            else
-                acc += ' ' + counter.to_s
-            end
-            counter += 1
-        end
-        acc
-    end
-end
 
 # END
